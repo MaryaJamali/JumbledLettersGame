d48111ba8893f329ae51c62362fa649ad1c63fbb
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-04-11 19:36:52 +0330

    Coding the displayGuidance function

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ const wordArray = [
     "chair", "table", "computer", "guitar", "book", "umbrella", "wristwatch", "bicycle", "tree", "flower",
     "wheat", "lion", "tiger", "elephant", "giraffe", "zebra", "penguin", "fish", "rooster", "rabbit",
     "pizza", "burger", "pasta", "sushi", "cake", "icecream", "sandwich", "soup", "salad",
-    "javascript", "python", "django", "html", "css", "react", "angular", "php", "nodejs", "mongodb"
+    "javascript", "python", "django", "html", "css", "react", "angular", "php", "nodejs", "mongodb",
+    "banana", "apple", "pineapple", "strawberry", "watermelon", "pear", "orange", "coconut", "blueberry", "cherry",
+    "red", "blue", "yellow", "pink", "purple", "green", "white", "black", "gray", "cream",
 ];
 
 // Game start mode
@@ -33,18 +35,19 @@ function gameStart() {
         // takes the correct word, separates it, mixes the letters using the desired function, and finally glues them together
         scrambled = randomArray(correctWord.split("")).join("");
         message.innerHTML = scrambled;
+        displayGuidance(correctWord);
     } else {
         // Get the user's guess
         let tempGuess = guess.value;
         score++;
         if (tempGuess === correctWord) {
             inPlay = false;
-            message.innerHTML = " Well done, you guessed right.the correct word was " + correctWord + " You could answer with " + score + " guesses";
+            message.innerHTML = " Well done, you guessed right.the correct word was ** " + correctWord + " **<br/> You could answer with " + score + " guesses";
             button.innerHTML = "Start Game";
             guess.classList.toggle("hidden");
         }
         else {
-            message.innerHTML = "Wow, the word you guessed " + scrambled + " It is wrong my friend.";
+            message.innerHTML = "Wow, the word you guessed ** " + scrambled + " ** It is wrong my friend.";
         }
 
     }
@@ -70,4 +73,47 @@ function randomArray(array) {
         array[w] = temp;
     }
     return array;
+}
+
+
+function displayGuidance(word) {
+
+    const girlArray = ["maryam", "reyhaneh", "leila", "sophia", "soraya", "darya", "giso", "fatima", "sahar", "zainab"];
+    const boyArray = ["ali", "reza", "amir", "mahmood", "morteza", "saeed", "sajad", "hossein", "nima", "abbas"];
+    const jobArray = ["doctor", "engineer", "artist", "teacher", "pilot", "chef", "programmer", "dentist", "photographer", "athlete"];
+    const thingArray = ["chair", "table", "computer", "guitar", "book", "umbrella", "wristwatch", "bicycle", "tree", "flower"];
+    const animalArray = ["wheat", "lion", "tiger", "elephant", "giraffe", "zebra", "penguin", "fish", "rooster", "rabbit"];
+    const foodArray = ["pizza", "burger", "pasta", "sushi", "cake", "icecream", "sandwich", "soup", "salad"];
+    const languageArray = ["javascript", "python", "django", "html", "css", "react", "angular", "php", "nodejs", "mongodb"];
+    const fruitArray = ["banana", "apple", "pineapple", "strawberry", "watermelon", "pear", "orange", "coconut", "blueberry", "cherry"];
+    const colorArray = ["red", "blue", "yellow", "pink", "purple", "green", "white", "black", "gray", "cream"];
+
+    // Checking the word in the desired array
+    if (girlArray.includes(word)) {
+        message.innerHTML += "<br/>Hint: This word is the girl's name.";
+    }
+    else if (boyArray.includes(word)) {
+        message.innerHTML += "<br/>Hint: This word is the boy's name.";
+    }
+    else if (jobArray.includes(word)) {
+        message.innerHTML += "<br/>Hint: This word is a job.";
+    }
+    else if (thingArray.includes(word)) {
+        message.innerHTML += "<br/>Hint: This word is a thing.";
+    }
+    else if (animalArray.includes(word)) {
+        message.innerHTML += "<br/>Hint: This word is a animal.";
+    }
+    else if (foodArray.includes(word)) {
+        message.innerHTML += "<br/>Hint: This word is a food.";
+    }
+    else if (languageArray.includes(word)) {
+        message.innerHTML += "<br/>Hint: This word is a language.";
+    }
+    else if (fruitArray.includes(word)) {
+        message.innerHTML += "<br/>Hint: This word is a fruit.";
+    }
+    else if (colorArray.includes(word)) {
+        message.innerHTML += "<br/>Hint: This word is a color.";
+    }
 }
