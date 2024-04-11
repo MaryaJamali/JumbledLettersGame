5824abe198b45ce993041b6aaf2180c06abc2263
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-04-11 16:57:20 +0330

    Coding the gameStart function

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@
 const message = document.querySelector(".guess-message");
 const guess = document.querySelector(".guess-input");
 const button = document.querySelector(".guess-btn");
+const Array = [
+    "maryam", "reyhaneh", "leila", "sophia", "soraya", "darya", "giso", "fatima", "sahar", "zainab",
+    "ali", "reza", "amir", "mahmood", "morteza", "saeed", "sajad", "hossein", "nima", "abbas",
+    "doctor", "engineer", "artist", "teacher", "pilot", "chef", "programmer", "dentist", "photographer", "athlete",
+    "chair", "table", "computer", "guitar", "book", "umbrella", "wristwatch", "bicycle", "tree", "flower",
+    "wheat", "lion", "tiger", "elephant", "giraffe", "zebra", "penguin", "fish", "rooster", "rabbit",
+    "pizza", "burger", "pasta", "sushi", "cake", "icecream", "sandwich", "soup", "salad",
+    "javascript", "python", "django", "html", "css", "react", "angular", "php", "nodejs", "mongodb"
+];
 // Game start mode
 let inPlay = false;
 // Add event to button
